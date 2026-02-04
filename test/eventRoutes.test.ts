d98794d79729a5b1a00fc690e1595aecf2454e4a
August <mturzak@rrc.ca>
2026-02-03 19:36:03 -0600

    Fianl Commit

--- test/eventRoutes.test.ts
+++ test/eventRoutes.test.ts
@@ -1,13 +1,32 @@
+jest.mock("src/api/v1/controllers/eventController");
+
 import request from "supertest";
 import express from "express";
 import eventRouter from "src/api/v1/routes/eventRoutes";
 import * as controller from "src/api/v1/controllers/eventController";
 
-jest.mock("src/api/v1/controllers/eventController");
 
 const app = express();
 app.use(express.json());
 app.use("/api/v1", eventRouter);
+
+(controller.getAllEvent as jest.Mock).mockImplementation((req, res) => {
+    res.status(200).json({ ok: true });
+});
+
+(controller.createEvent as jest.Mock).mockImplementation((req, res) => {
+    res.status(201).json({ ok: true });
+});
+
+(controller.updateEvent as jest.Mock).mockImplementation((req, res) => {
+    res.status(200).json({ ok: true });
+});
+
+(controller.deleteEvent as jest.Mock).mockImplementation((req, res) => {
+    res.status(204).send();
+});
+
+
 
 describe("Route EventName", () => {
 	afterEach(() => {
